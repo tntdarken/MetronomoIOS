7ad9d68059aa5cc7fedb20e37c4bb86b2c790f28
Arthur Luiz Lara Quites <arthur.quites@unyleya.com.br>
2020-01-17 14:19:31 -0300

    Initial Commit

---
+++ MetronomoIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetronomoIOS
+//
+//  Created by Arthur Luiz Lara Quites on 17/01/20.
+//  Copyright © 2020 Arthur Luiz Lara Quites. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
